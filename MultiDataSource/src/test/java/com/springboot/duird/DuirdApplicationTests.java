0ea083d964f9fd592aa299bb333ad1f1e3dfac69
Yingjie <yingjie.guan@thoughtworks.com>
2020-11-10 22:57:40 +0800

    yingjie/add durid project

---
+++ MultiDataSource/src/test/java/com/springboot/duird/DuirdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.duird;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuirdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
